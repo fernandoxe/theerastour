d95f6e02b9acd4d21efe9d175f761243fdfaa694
Fernando Parra <fernandoxe@gmail.com>
2022-11-03 20:32:28 -0300

    chore: setlist

--- src/components/Setlist/index.tsx
+++ src/components/Setlist/index.tsx
@@ -1,4 +1,17 @@
-import { Reorder } from 'framer-motion';
+import {
+  DndContext,
+  closestCenter,
+  MouseSensor,
+  TouchSensor,
+  useSensor,
+  useSensors,
+} from '@dnd-kit/core';
+import {
+  arrayMove,
+  SortableContext,
+  verticalListSortingStrategy,
+} from '@dnd-kit/sortable';
+import { restrictToVerticalAxis } from '@dnd-kit/modifiers';
 import { Track } from '../../interfaces/Spotify';
 import { Button } from '../Button';
 import { Item } from './Item';
@@ -10,22 +23,38 @@ export interface SetlistProps {
 }
 
 export const Setlist = ({ selectedTracks, onReorder, onCreatePlaylist }: SetlistProps) => {
-  
+  const sensors = useSensors(
+    useSensor(TouchSensor),
+    useSensor(MouseSensor),
+  );
+
+  const handleDragEnd = (event: any) => {
+    const {active, over} = event;
+    
+    if (active.id !== over.id) {
+      const oldIndex = selectedTracks.findIndex((track => track.id === active.id));
+      const newIndex = selectedTracks.findIndex((track => track.id === over.id));
+
+      const newSelectedTracks = arrayMove(selectedTracks, oldIndex, newIndex);
+      onReorder(newSelectedTracks);
+    }
+  }
 
   return (
     <>
       <h3 className="font-bold text-base mb-4">The Eras Tour setlist</h3>
-      <div className="flex flex-col">
-        <Reorder.Group
-          axis="y"
-          values={selectedTracks}
-          onReorder={onReorder}
-        >
+      <DndContext
+        sensors={sensors}
+        collisionDetection={closestCenter}
+        onDragEnd={handleDragEnd}
+        modifiers={[restrictToVerticalAxis]}
+      >
+        <SortableContext items={selectedTracks} strategy={verticalListSortingStrategy}>
           {selectedTracks.map((track, i) =>
             <Item key={track.id} track={track} position={i + 1} />
           )}
-        </Reorder.Group>
-      </div>
+        </SortableContext>
+      </DndContext>
       <div className="flex justify-center gap-4">
         <Button
           onClick={onCreatePlaylist}
