d85215d6bc331843de3826e65a44511290e95b5e
Fernando Parra <fernandoxe@gmail.com>
2022-11-03 20:32:08 -0300

    chore: item

--- src/components/Setlist/Item/index.tsx
+++ src/components/Setlist/Item/index.tsx
@@ -1,5 +1,6 @@
-import { Reorder, useDragControls } from 'framer-motion';
 import { Track } from '../../../interfaces/Spotify';
+import { useSortable } from '@dnd-kit/sortable';
+import { CSS } from '@dnd-kit/utilities';
 import { ReactComponent as Logo } from '../../../icons/drag.svg';
 
 export interface ItemProps {
@@ -8,23 +9,32 @@ export interface ItemProps {
 }
 
 export const Item = ({ track, position }: ItemProps) => {
-  const dragControls = useDragControls();
+  const {
+    attributes,
+    listeners,
+    setNodeRef,
+    setActivatorNodeRef,
+    transform,
+    transition,
+  } = useSortable({ id: track.id });
+
+  const style = {
+    transform: CSS.Transform.toString(transform),
+    transition,
+  };
 
   return (
-    <Reorder.Item
-      key={track.id}
-      value={track}
-      dragListener={false}
-      dragControls={dragControls}
-    >
-      <div className="flex px-4 py-2 text-sm border bg-white border-slate-400 rounded-lg mb-2 select-none">
+    <div ref={setNodeRef} style={style}>
+      <div className="flex items-center p-2 text-sm border bg-white border-slate-400 rounded-lg mb-2 select-none">
         <div
           className="w-5 h-5 mr-2"
-          onPointerDown={e => dragControls.start(e)}
+          ref={setActivatorNodeRef}
+          {...attributes}
+          {...listeners}
         >
           <Logo />
         </div>
-        <div className="flex gap-2">
+        <div className="flex items-center gap-2">
           <div>
             {position}
           </div>
@@ -33,6 +43,6 @@ export const Item = ({ track, position }: ItemProps) => {
           </div>
         </div>
       </div>
-    </Reorder.Item>
+    </div>
   );
 };
