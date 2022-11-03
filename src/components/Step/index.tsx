43baa45f3e66030f4dfdd536cb091a795aea0b8d
Fernando Parra <fernandoxe@gmail.com>
2022-11-02 21:59:34 -0300

    chore: step

--- src/components/Step/index.tsx
+++ src/components/Step/index.tsx
@@ -1,13 +1,29 @@
-import { FC, ReactNode } from 'react';
+import { Album } from '../../interfaces/Spotify';
+import { Track } from '../Track';
 
 export interface StepProps {
-  children: ReactNode,
+  album: Album;
+  checkedState: boolean[];
+  onChange: (checkedState: boolean[]) => void;
 }
 
-export const Step: FC<StepProps> = ({ children }) => {
+export const Step = ({ album, checkedState, onChange }: StepProps) => {
+  const handleChecked = (trackNumber: number) => {
+    const newCheckedState = checkedState.map((item, i) => i === trackNumber ? !item : item);
+    if(newCheckedState.filter(item => item).length > 3) return;
+    onChange(newCheckedState);
+  };
+
   return (
-    <div>
-      {children}
-    </div>
+    <div className="flex flex-col gap-4">
+      <h3 className="font-bold text-base">{album.name}</h3>
+      {album.tracks.map((track, i) =>
+        <Track
+          checked={checkedState[i]}
+          name={track.name}
+          onChange={() => handleChecked(i)}
+        />
+      )}
+      </div>
   )
 };
