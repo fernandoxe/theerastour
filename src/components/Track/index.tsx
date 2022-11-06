0923f841191e5c0558c8adc33fd1a1be09a75414
fernandoxe <fernandoxe@gmail.com>
2022-11-05 21:06:41 -0700

    chore: position and siabled

--- src/components/Track/index.tsx
+++ src/components/Track/index.tsx
@@ -1,20 +1,30 @@
 export interface TrackProps {
   checked: boolean;
   name: string;
-  onChange: () => void;
+  position?: number;
+  disabled?: boolean;
+  onChange?: () => void;
 }
 
-export const Track = ({ checked, name, onChange }: TrackProps) => {
+export const Track = ({ checked, name, position, disabled, onChange }: TrackProps) => {
   return (
     <label>
       <input
         type="checkbox"
         className="peer sr-only"
         checked={checked}
+        disabled={disabled}
         onChange={onChange}
       />
-      <div className="px-4 py-2 text-white text-sm font-semibold shadow-sm shadow-[rgba(34,41,69,0.6)] peer-checked:bg-[#525b84] peer-checked:shadow-none border border-[#525b84] rounded-lg select-none">
-        {name}
+      <div className="flex items-center gap-2 px-4 py-2 text-white text-sm shadow-sm shadow-[rgba(34,41,69,0.6)] peer-checked:bg-[#525b84] peer-checked:shadow-none border border-[#525b84] rounded-lg select-none">
+        {position &&
+          <div className="text-xs">
+            {position}
+          </div>
+        }
+        <div className="font-semibold">
+          {name}
+        </div>
       </div>
     </label>
   );
